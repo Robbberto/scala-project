cf75eba1415e9c8ad227c1a0049f98f05349d312
Roberto Ceraolo <45052083+Robbberto@users.noreply.github.com>
2022-03-09 19:01:49 +0400

    new functions included, now can handle the MAE computation

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -2,7 +2,7 @@ import scala.io.Source
 
 object HelloWorld {
 
-  def userAvgRat(user: Int, valuesAsList: List[(Int, Int, Int)]): Double = {
+  def computeUserAvgRat(user: Int, valuesAsList: List[(Int, Int, Int)]): Double = {
 /*
 Function that computes the average rating for a user. In case the user did not rate any movie, it returns 0
 */
@@ -16,7 +16,7 @@ Function that computes the average rating for a user. In case the user did not r
     }
   }
 
-  def itemAvgRat(item: Int, valuesAsList: List[(Int, Int, Int)]): Double = {
+  def computeItemAvgRat(item: Int, valuesAsList: List[(Int, Int, Int)]): Double = {
 /*
 Function that computes the average rating of a movie. In case the movie has no reviews, it returns 0
 */
@@ -59,7 +59,7 @@ Prediction function as in formula 5 in the project description
 
 
 
-  def globalAvgDev(item: Int, valuesAsList: List[(Int, Int, Int)]): Double = { 
+  def computeGlobalAvgDev(item: Int, valuesAsList: List[(Int, Int, Int)]): Double = { 
 /*
 Function that computes the global average deviation for a given item
 */
@@ -75,7 +75,7 @@ Function that computes the global average deviation for a given item
 	  return 0.0
 	} else {
 	  val deviations = for (review <- itemData;
-				avgRat = userAvgRat(review._1, valuesAsList);
+				avgRat = computeUserAvgRat(review._1, valuesAsList);
 				deviation = (review._3 - avgRat) / scale(review._3, avgRat)
 			     ) yield (review._1, deviation)
 
@@ -84,41 +84,126 @@ Function that computes the global average deviation for a given item
 	}
   }
 
+    def computeGlobalAvg(valuesAsList: List[(Int, Int, Int)]): Double = {
+	return valuesAsList.map(_._3).sum/valuesAsList.size.toDouble
+  }
+
+    def computePredictionsGlobalAvg(testValuesAsList: List[(Int, Int, Int)], trainValuesAsList: List[(Int, Int, Int)]): Double = {
+/*
+Function that does the predictions using Global Average on all the elements of the test set. Training is done with the training set.
+*/
+	  val globalAvg = computeGlobalAvg(trainValuesAsList)
+	// predictions è una lista di tuple (valore vero, valore predetto)
+	  println("Number of values to predict: " + testValuesAsList.size)
+
+	  val predictions = for ((review, i) <- testValuesAsList.zipWithIndex;
+				//println("Prediction " + i);
+				prediction = globalAvg
+			     ) yield (review._3, prediction)
+
+	  return predictions
+
+}
+
+    def computePredictionsItemAvg(testValuesAsList: List[(Int, Int, Int)], trainValuesAsList: List[(Int, Int, Int)]): Double = {
+/*
+Function that does the predictions using Item Averages on all the elements of the test set. Training is done with the training set.
+*/
+	// predictions è una lista di tuple (valore vero, valore predetto)
+	  println("Number of values to predict: " + testValuesAsList.size)
+
+	  val predictions = for ((review, i) <- testValuesAsList.zipWithIndex;
+				//println("Prediction " + i);
+				prediction = computeItemAvgRat(review._2, trainValuesAsList)
+			     ) yield (review._3, prediction)
+
+	  return predictions
+
+}
+
+
+    def computePredictionsUserAvg(testValuesAsList: List[(Int, Int, Int)], trainValuesAsList: List[(Int, Int, Int)]): Double = {
+/*
+Function that does the predictions using Item Averages on all the elements of the test set. Training is done with the training set.
+*/
+	// predictions è una lista di tuple (valore vero, valore predetto)
+	  println("Number of values to predict: " + testValuesAsList.size)
+
+	  val predictions = for ((review, i) <- testValuesAsList.zipWithIndex;
+				//println("Prediction " + i);
+				prediction = computeUserAvgRat(review._1, trainValuesAsList)
+			     ) yield (review._3, prediction)
+
+	  return predictions
+
+}
+
+
+
+
+   def computePredictionsEq5(testValuesAsList: List[(Int, Int, Int)], trainValuesAsList: List[(Int, Int, Int)]): Double = {
+/*
+Function that does the predictions using equation 5 on all the elements of the test set. Training is done with the training set.
+*/
+	  val globalAvg = computeGlobalAvg(trainValuesAsList)
+	// predictions è una lista di tuple (valore vero, valore predetto)
+	  println("Number of values to predict: " + testValuesAsList.size)
+
+	  val predictions = for ((review, i) <- testValuesAsList.zipWithIndex;
+				//println("Prediction " + i);
+				avgRat = computeUserAvgRat(review._1, trainValuesAsList);
+				avgDev = computeGlobalAvgDev(review._2, trainValuesAsList);
+				prediction = predict(avgRat, avgDev, globalAvg)
+			     ) yield (review._3, prediction)
+
+	  return predictions
+
+}
+
+
+
+
+   def computeMae(predictions: List[(Int, Double)]): Double = {
+	// ora calcoliamo il MAE
+	val errors = for (couple <- predictions;
+	  	       	increment = (couple._2 - couple._1).abs
+	  		) yield increment 
+	  var mae = errors.sum
+	  mae = mae / testValuesAsList.size
+	  return mae
+   }
+
+
+   def extractValues(path: String): List[(Int, Int, Int)] = {
+     val parsed =
+            for (line<-Source.fromFile(path).getLines;
+                 Array(user, movie, rating, timestamp) = line.split("\t")
+            ) yield (user.toInt, movie.toInt, rating.toInt)// rimuovo il timestamp
+     val valuesAsList = parsed.toList
+     return valuesAsList
+
+   }
+
 
   def main(args: Array[String]) {
 /*
 Main function that handles everything
 */
+    val trainDataPath = "/Users/rob/Documents/EPFL/systems/data/ml-100k/u2.base"
+    val trainValuesAsList = extractValues(trainDataPath)
 
-    var filename = "/Users/rob/Documents/EPFL/systems/data/ml-100k/u2.base"
-    val parsed =
-            for (line<-Source.fromFile(filename).getLines;
-                 Array(user, movie, rating, timestamp) = line.split("\t")
-            ) yield (user.toInt, movie.toInt, rating.toInt)// rimuovo il timestamp
-    val valuesAsList = parsed.toList
-
-
-
-/* Versione vecchia, senza usare la funzione apposita (può essere eliminata, mantengo temporaneamente per reference
-
-    val user1Data = valuesAsList.filter(x=>x._1 == 1)
-    val avgRatingUser1 = user1Data.map(_._3).sum/user1Data.size.toDouble
-    
-    val item1Data = valuesAsList.filter(x=>x._2 == 1)
-    val avgRatingItem1 = item1Data.map(_._3).sum/item1Data.size.toDouble
-
-    println("Average rating user 1: " + avgRatingUser1)
-    println("Average rating item 1: " + avgRatingItem1)
-
-*/
 
     // QUESTION B1
-    val globalAvg = valuesAsList.map(_._3).sum/valuesAsList.size.toDouble
+    val globalAvgWithFunct = computeGlobalAvg(trainValuesAsList)
+    val globalAvg = trainValuesAsList.map(_._3).sum/trainValuesAsList.size.toDouble
     println("Global average: " + globalAvg)
+    println("Global average with function: " + globalAvgWithFunct)
 
-    val avgRatUser1 = userAvgRat(1, valuesAsList)
-    val avgRatItem1 = itemAvgRat(1, valuesAsList)
-    val avgDevItem1 = globalAvgDev(1, valuesAsList)
+
+
+    val avgRatUser1 = computeUserAvgRat(1, trainValuesAsList)
+    val avgRatItem1 = computeItemAvgRat(1, trainValuesAsList)
+    val avgDevItem1 = computeGlobalAvgDev(1, trainValuesAsList)
 
     println("Average rating user 1 with function: " + avgRatUser1)
     println("Average rating item 1 with function: " + avgRatItem1)
@@ -127,7 +212,60 @@ Main function that handles everything
 
     println("Prediction user 1, item 1: " + predict(avgRatUser1, avgDevItem1, globalAvg))
 
+
     // QUESTION B2
+
+    val testDataPath = "/Users/rob/Documents/EPFL/systems/data/ml-100k/u2.test"
+    val testValuesAsList = extractValues(testDataPath)
+
+    //mae with baseline prediction function
+    val starting_time = System.nanoTime()
+    val predictions = computePredictionsEq5(testValuesAsList, trainValuesAsList)
+    val maePredictionFunct = computeMae(predictions)
+    val timing = System.nanoTime() - starting_time
+    val resultBaseline: Seq[Double] = Seq(mae, timing)
+
+
+    //mae with global average
+    val starting_time = System.nanoTime()
+    val predictions = computePredictionsGlobalAvg(testValuesAsList, trainValuesAsList)
+    val maeGlobalAvg = computeMae(predictions)
+    val timing = System.nanoTime() - starting_time
+    val resultGlobalAvg: Seq[Double] = Seq(mae, timing)
+
+
+    //mae with average ratings for users
+    val starting_time = System.nanoTime()
+    val predictions = computePredictionsUserAvg(testValuesAsList, trainValuesAsList)
+    val maeUserAvg = computeMae(predictions)
+    val timing = System.nanoTime() - starting_time
+    val resultUserAvg: Seq[Double] = Seq(mae, timing)
+
+    //mae with average ratings for items
+    val starting_time = System.nanoTime()
+    val predictions = computePredictionsItemAvg(testValuesAsList, trainValuesAsList)
+    val maeItemAvg = computeMae(predictions)
+    val timing = System.nanoTime() - starting_time
+    val resultItemAvg: Seq[Double] = Seq(mae, timing)
+
+
+    println("MAE with baseline prediction function: " + maePredictionFunct)
+    println("Time needed: " + resultBaseline._1)
+    println("\n")
+
+    println("MAE with global average: " + maeGlobalAvg)
+    println("Time needed: " + resultGlobalAvg._1)
+    println("\n")
+
+    println("MAE with average ratings for users: " + maeUserAvg)
+    println("Time needed: " + resultUserAvg._1)
+    println("\n")
+
+    println("MAE with average ratings for items: " + maeItemAvg)
+    println("Time needed: " + resultItemAvg._1)
+    println("\n")
+
+
 
 
   }
